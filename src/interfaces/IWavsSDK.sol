ca63639225f841e098a8c88411af4bafdc552edd
Meow <jacobhartnell@Meow.local>
2025-01-25 19:07:07 +0100

    Merge SDKs

--- src/interfaces/IWavsSDK.sol
+++ src/interfaces/IWavsSDK.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.22;
 
 import {ECDSAStakeRegistry} from "@eigenlayer/middleware/src/unaudited/ECDSAStakeRegistry.sol";
+import {IWavsTrigger} from "./IWavsTrigger.sol";
 
 interface IWavsSDK {
     struct SignedPayload {
@@ -27,4 +28,14 @@ interface IWavsSDK {
      * @return _stakeRegistry The stake registry address
      */
     function STAKE_REGISTRY() external view returns (ECDSAStakeRegistry _stakeRegistry); // solhint-disable-line func-name-mixedcase
+
+    // From ISimpleSubmit
+    struct DataWithId {
+        IWavsTrigger.TriggerId triggerId;
+        bytes data;
+    }
+
+    // just to make alloy see the DataWithId struct
+    function getDataWithId(IWavsTrigger.TriggerId triggerId) external view returns (DataWithId memory);
+
 }
--- src/interfaces/IWavsTrigger.sol
+++ src/interfaces/IWavsTrigger.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.22;
 
 interface IWavsTrigger {
     event WavsTriggerEvent(bytes);
+
+    // From ISimpleTrigger
+    struct TriggerInfo {
+        TriggerId triggerId;
+        address creator;
+        bytes data;
+    }
+
+    type TriggerId is uint64;
+
+    function getTrigger(
+        TriggerId triggerId
+    ) external view returns (TriggerInfo memory);
 }
